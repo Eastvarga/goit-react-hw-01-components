19713f5ffcc52a417a9f75bf4cb6d9865db810e5
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-03-21 12:59:55 +0200

    First task Done

---
+++ src/components/Profile.js
@@ -0,0 +1,49 @@
+import defaultImage from "./User-Default.jpg";
+import PropTypes from "prop-types";
+
+const Profile = ({ name, tag, location, avatar, stats }) => {
+  return (
+    <div className="profile">
+      <div className="description">
+        <img src={avatar} alt={name} className="avatar" width="240" />
+        <p className="name">{name}</p>
+        <p className="tag">&#64;{tag}</p>
+        <p className="location">{location}</p>
+      </div>
+
+      <ul className="stats">
+        <li>
+          <span className="label">Followers: </span>
+          <span className="quantity">{stats.followers}</span>
+        </li>
+        <li>
+          <span className="label">Views: </span>
+          <span className="quantity">{stats.views}</span>
+        </li>
+        <li>
+          <span className="label">Likes: </span>
+          <span className="quantity">{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+Profile.defaultProps = {
+  name: "nobody",
+  avatar: defaultImage,
+  tag: "empty",
+  stats: {
+    followers: 0,
+    views: 0,
+    likes: 0,
+  },
+};
+
+Profile.propTypes = {
+  name: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  stats: PropTypes.object.isRequired,
+};
+export default Profile;
